2a2bd94a8b29827ce8433068996468d248b41ca2
Joaquín Hernández <joaquin_downhill@hotmail.com>
2024-06-05 16:22:43 -0600

    Sidebar, header and main content

---
+++ FRONTEND/src/app/core/components/sidebar/sidebar.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.css']
+})
+export class SidebarComponent implements OnInit {
+
+  collapseShow = "hidden";
+
+  ngOnInit(): void {
+    
+  }
+
+  toggleCollapseShow(classes:any) {
+    this.collapseShow = classes;
+  }
+}
--- FRONTEND/src/app/modules/main/main.module.ts
+++ FRONTEND/src/app/modules/main/main.module.ts
@@ -2,12 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MainComponent } from './main/main.component';
+import { SidebarComponent } from './../../core/components/sidebar/sidebar.component';
+import { NavbarComponent } from './../../core/components/navbar/navbar.component';
+import { HeaderComponent } from './../../core/components/header/header.component';
 import { MainRoutingModule } from './main-routing.module';
 
 
 @NgModule({
   declarations: [
-    MainComponent
+    MainComponent,
+    SidebarComponent,
+    NavbarComponent,
+    HeaderComponent
   ],
   imports: [
     CommonModule,
