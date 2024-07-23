bb2bdd779ec939afb34790564dec6ce492c03a64
Joaquín Hernández <joaquin_downhill@hotmail.com>
2024-07-23 17:34:42 -0600

    Update environment.development.ts

--- FRONTEND/src/environments/environment.development.ts
+++ FRONTEND/src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    //server: "http://217.15.168.147:5001",
-    server: "http://localhost:5001"
+    server: "http://217.15.168.147:5001",
+    //server: "http://localhost:5001"
 };
